3eb931590697585f9a4854fb8d693caaadaad77a
stoennnnn <2672575920@qq.com>
2019-01-10 16:56:51 +0800

    Initial commit

---
+++ src/main/java/com/zql/Application.java
@@ -0,0 +1,14 @@
+package com.zql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 26725 on 2019/1/10.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
